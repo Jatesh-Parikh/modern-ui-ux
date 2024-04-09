3df756d52c435a784a32132fe47e9901f357199b
Jatesh Parikh <jateshparikh97@gmail.com>
2024-04-09 20:31:05 +0530

    project completed

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import { Features } from "./components/Features";
 import { Hero } from "./components/Hero";
 import { Navbar } from "./components/Navbar";
 import { Pricing } from "./components/Pricing";
+import { Cta } from "./components/Cta";
+import { Footer } from "./components/Footer";
 
 export default function Home() {
   return (
@@ -13,6 +15,8 @@ export default function Home() {
         <Features />
         <Faq />
         <Pricing />
+        <Cta />
+        <Footer />
       </div>
     </>
   );
